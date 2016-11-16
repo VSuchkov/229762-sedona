d69e51c73bb84e283549a55cbce88d776f3b2c45
VSuchkov <ssvovs@mail.ru>
2016-11-16 19:32:09 +0300

    лоработочки

--- js/script.js
+++ js/script.js
@@ -10,68 +10,70 @@ var adultsMinus = popup.querySelector(".adults-minus");
 var childrenMinus = popup.querySelector(".children-minus");
 var childrenGuests = popup.querySelector("#children");
 var adultsGuests = popup.querySelector("#adults");
-      
+
+popup.classList.add("order-form-hidden");
+
 adultsMinus.addEventListener("click", function(event) {
   event.preventDefault();
-	adultsGuests.value = parseInt(adultsGuests.value) - 1;
-	if(adultsGuests.value < 0) {
-    adultsGuests.value = 0;
+    adultsGuests.value = parseInt(adultsGuests.value) - 1;
+    if(adultsGuests.value < 0) {
+      adultsGuests.value = 0;
     }
   });
-      
+
   adultsPlus.addEventListener("click", function(event) {
   event.preventDefault();
-	adultsGuests.value = parseInt(adultsGuests.value) + 1;
-	if (adultsGuests.value > 9) {
-    adultsGuests.value = 9;
+    adultsGuests.value = parseInt(adultsGuests.value) + 1;
+    if (adultsGuests.value > 9) {
+      adultsGuests.value = 9;
     }
-	 });
-      
-   childrenMinus.addEventListener("click", function(event) {
-	   event.preventDefault();
-	   childrenGuests.value = parseInt(childrenGuests.value) - 1;
-	   if(childrenGuests.value < 0) {
-      childrenGuests.value = 0;
-      }
-	   });
-      
-    childrenPlus.addEventListener("click", function(event) {
-		  event.preventDefault();
-		  childrenGuests.value = parseInt(childrenGuests.value) + 1;
-		  if (childrenGuests.value > 9) {
-        childrenGuests.value = 9;
-        }
-	    });
-      
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.toggle("order-form-show");
-      popup.classList.remove("modal-error");
-      if (storage) {
-        dateEnter.value = storage;
-        dateOut.focus();
-      } else {
-        dateEnter.focus();
-        }
-      });
-      
-    form.addEventListener("submit", function(event){
-      if (!dateEnter.value || !dateOut.value) {
-      event.preventDefault();
-      popup.classList.remove("modal-error");
-      popup.offsetWidth = popup.offsetWidth;
-      popup.classList.add("modal-error");
-      console.log("Нужно ввести логин и пароль!");
-      } else {
-      localStorage.setItem("dateEnter", dateEnter.value);
-      }
-    });
+  });
 
-    window.addEventListener("keydown", function(event) {
-      if (event.keyCode === 27) {
-        if (popup.classList.contains("order-form-show")) {
-          popup.classList.remove("order-form-show");
-          popup.classList.remove("modal-error");
-        }
+  childrenMinus.addEventListener("click", function(event) {
+    event.preventDefault();
+    childrenGuests.value = parseInt(childrenGuests.value) - 1;
+    if(childrenGuests.value < 0) {
+      childrenGuests.value = 0;
+    }
+  });
+
+  childrenPlus.addEventListener("click", function(event) {
+    event.preventDefault();
+    childrenGuests.value = parseInt(childrenGuests.value) + 1;
+    if (childrenGuests.value > 9) {
+      childrenGuests.value = 9;
+    }
+  });
+
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.toggle("order-form-show");
+    popup.classList.remove("modal-error");
+    if (storage) {
+      dateEnter.value = storage;
+      dateOut.focus();
+    } else {
+    dateEnter.focus();
+    }
+  });
+
+  form.addEventListener("submit", function(event){
+    if (!dateEnter.value || !dateOut.value) {
+    event.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    console.log("Нужно ввести логин и пароль!");
+    } else {
+    localStorage.setItem("dateEnter", dateEnter.value);
+    }
+  });
+
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (popup.classList.contains("order-form-show")) {
+        popup.classList.remove("order-form-show");
+        popup.classList.remove("modal-error");
       }
-    });
+    }
+  });
